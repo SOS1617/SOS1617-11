533169d0549a109d92b9262e3afe80bc88874284
sos161711apg <adrianperezgutierrez@hotmail.com>
2017-03-07 02:29:59 +0000

    Commit index.html

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 var express = require("express");
 var date = require("fecha");
-
-var port = (process.env.PORT || 16778); /*Si esta definida port se coge port, sino el puerto que queramos*/
+var path = require("path");
 
 var app = express();
+var port = (process.env.PORT || 16778);
 
 
 
+app.use("/",express.static( path.join( __dirname,"public")));
 
 app.get("/time",(req,res) =>{
     res.send("<html><body><h1>" + date.format(new Date(Date.now()),'Do MMMM [of] YYYY [,] HH:mm:ss') + "</h1></body></html>");
 });
 
 
-app.get("/", (req, res) => {
+/*app.get("/", (req, res) => {
     res.send("<html><body><h1><a href=/time>/time</a></h1></body><html>");
 });
-
-app.listen(port,(err) => {
-    if(!err)
+*/
+app.listen(port,() => {
         console.log("Server inicialized on port: "+port);
-    else
-        console.log("ERROR inicializing server on port: "+port+ ": "+err);
+}).on("error",(e) =>{
+    console.log("ERROR inicializing server on port: "+port+ ": "+e);
+    process.exit(1);
 });

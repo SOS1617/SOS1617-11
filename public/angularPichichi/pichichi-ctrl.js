215365e48f3dd8faf58db8767e62078d8ef47ae9
sos161711dbd <davidbonilla37@gmail.com>
2017-04-18 19:57:26 +0000

    nueva version

--- public/angularPichichi/pichichi-ctrl.js
+++ public/angularPichichi/pichichi-ctrl.js
@@ -8,7 +8,7 @@ angular
     
     
     //CARGAR DATOS
-        $scope.loadInitialData= function(){
+      /*  $scope.loadInitialData= function(){
             $http.get("/lfppichichitrophy"+"/loadInitialData?apikey="+$scope.apikey)
             .then(function(){
                 console.log("Load initial data: OK");
@@ -19,14 +19,14 @@ angular
     function refresh(){
          $http
             .get("/lfppichichitrophy"+"?apikey="+ $scope.apikey)
-            /*.get("api/v1/lfppichichitrophy")*/
+            /*.get("api/v1/lfppichichitrophy")
             .then(function (response){
                 $scope.data = JSON.stringify(response.data, null, 2);
                 $scope.lfppichichitrophy = response.data;
             }, function errorCallback(response){
                 console.log("Error callback");
             });
-    }
+    }*/
     
     
     //AÑADIR UN NUEVO PICHICHI

e445b1cde1f6f572c7338b0bb9d417bca5196644
sos161711apg <adrianperezgutierrez@hotmail.com>
2017-04-23 01:38:03 +0000

    fdvf

---
+++ public/angularUclchampions/uclchampions-ctrl.js
@@ -0,0 +1,117 @@
+//////////////SE INICIALIZA EL CONTROLADOR ////////////////////////
+
+angular
+    .module("UclchampionsManagerApp")
+    .controller("uclchampions-ctrl", ["$scope", "$http", function ($scope, $http){
+    
+     $scope.url = "/api/v1/uclchampions";
+    
+    console.log("Controller initialized (splited right)");
+    
+    
+    //CARGAR DATOS
+       $scope.loadInitialData= function(){
+            $http.get($scope.url+"/loadInitialData?apikey="+$scope.apikey)
+            .then(function(){
+                console.log("Load initial data: OK");
+                refresh();
+            })
+        }
+   function refresh(){
+         $http
+            .get($scope.url+"?apikey="+ $scope.apikey)
+            /*.get("api/v1/lfppichichitrophy")*/
+            .then(function (response){
+                $scope.data = JSON.stringify(response.data, null, 2);
+                $scope.uclchampions = response.data;
+            }, function errorCallback(response){
+                console.log("Error callback");
+            });
+    }
+    
+    //GET
+     $scope.getData = function(){
+            $http
+            .get($scope.url+"?apikey="+ $scope.apikey)
+            .then(function(response){
+               $scope.uclchampions= response.data;
+                console.log( "Showing data " );
+
+                });
+                
+              } 
+    
+    //PAGINACIÓN
+    
+    $scope.offset = 0;
+     $scope.getPaginacion = function(){
+           
+            $http
+                .get($scope.url+"?apikey="+ $scope.apikey +"&limit="+ $scope.limit +"&offset="+$scope.offset)
+                .then(function(response){
+                    $scope.data = JSON.stringify(response.data, null, 2); 
+                    $scope.uclchampions = response.data;
+                    console.log( $scope.data );
+                });
+            
+        } ;
+    
+    
+    //AÑADIR UN NUEVO PICHICHI
+    $scope.adduclchampion = function (){
+        var datainput = $scope.newuclchampions;
+        console.log(datainput);
+        $http
+            .post($scope.url+"?apikey="+ $scope.apikey,datainput)
+            .then(function (response){
+                console.log("Contact added");
+                refresh();
+        });
+    }
+    
+    
+   
+        //MODIFICAR UN PICHICHI
+        $scope.editauclchampion = function(){
+            $http.put($scope.url +"/" + $scope.newuclchampions.year +"?apikey=" + $scope.apikey,$scope.newuclchampions)
+            .then(function(response){
+                console.log("Campeon modificadao correctamente");
+                refresh();
+            });
+            
+        }
+     
+    //ELIMINAR UN PICHICHI
+     $scope.deleteuclchampion = function (year){
+        $http
+            .delete($scope.url+"/" + year +"/?apikey="+ $scope.apikey)
+            .then(function (response){
+                console.log("Campeon del año:" + year + "eliminado");
+                refresh();
+        });
+    }
+    
+   
+    //ELIMINAR LA LISTA
+    $scope.deleteLista = function(){
+            $http
+                .delete($scope.url+"?apikey="+ $scope.apikey)
+                .then(function(response){
+                    console.log("LISTA VACÍA");
+                    refresh();
+                });
+        }
+      
+        //BÚSQUEDA
+        $scope.search = function(){
+            $http
+                .get($scope.url+"?apikey="+$scope.apikey+"&year="+$scope.newuclchampions.year)
+                .then(function(response){
+                    console.log("Muestra el campeon del año: " + $scope.newuclchampions.year);
+                    $scope.data = JSON.stringify(response.data, null, 2);
+                    $scope.uclchampions = response.data; 
+                });
+        }
+        
+           
+    }]);
---
+++ public/angularUclchampions/uclchampions-manager.js
@@ -0,0 +1,6 @@
+  angular.module("UclchampionsManagerApp", []);
+    /*Si no ponemos el array vacio [] no estamos creando un módulo, si no 
+    que estamo sdiciendole a angular que nos de el modulo con ese nombre*/
+    console.log("App inizialized");
+    
+  

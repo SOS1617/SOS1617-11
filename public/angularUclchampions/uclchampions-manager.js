45eb48ca1d76a56acbabe88c31f5eee16b815eb6
sos161711apg <adrianperezgutierrez@hotmail.com>
2017-04-25 15:03:05 +0000

    scd

--- public/angularUclchampions/uclchampions-manager.js
+++ public/angularUclchampions/uclchampions-manager.js
@@ -1,6 +1,17 @@
-  angular.module("UclchampionsManagerApp", []);
+  angular.module("UclchampionsManagerApp", ["ngRoute"].config(function ($routeProvider){
+    
+      $routeProvider.when("/",{
+        templateUrl : "list.html" ,
+        controller : "Uclchampion-ctrl"
+      });
+    
+    
+    
+    
+      console.log("App inizialized and configured");
+
+  }));
     /*Si no ponemos el array vacio [] no estamos creando un módulo, si no 
     que estamo sdiciendole a angular que nos de el modulo con ese nombre*/
-    console.log("App inizialized");
     
   
